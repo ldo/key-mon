2f8440452072bcd1f189595f52536362bac63a5e
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2020-06-29 03:31:01 +0000

    finish reformatting job

--- src/keymon/two_state_image.py
+++ src/keymon/two_state_image.py
@@ -31,117 +31,117 @@ from gi.repository import \
 DEFAULT_TIMEOUT_SECS = 0.5
 
 class TwoStateImage(Gtk.Image):
-  """Image has a default image (say a blank image) which it goes back to.
-  It can also pass the information down to another image."""
+    """Image has a default image (say a blank image) which it goes back to.
+    It can also pass the information down to another image."""
 
-  def __init__(self, pixbufs, normal, show=True, defer_to=None):
-      Gtk.Image.__init__(self)
-      self.pixbufs = pixbufs
-      self.normal = normal
-      self.count_down = None
-      self.showit = show
-      self.current = ''
-      self.defer_to = defer_to
-      self.timeout_secs = DEFAULT_TIMEOUT_SECS
-      self.switch_to(self.normal)
-      self._really_pressed = False
-  #end __init__
+    def __init__(self, pixbufs, normal, show=True, defer_to=None):
+        Gtk.Image.__init__(self)
+        self.pixbufs = pixbufs
+        self.normal = normal
+        self.count_down = None
+        self.showit = show
+        self.current = ''
+        self.defer_to = defer_to
+        self.timeout_secs = DEFAULT_TIMEOUT_SECS
+        self.switch_to(self.normal)
+        self._really_pressed = False
+    #end __init__
 
-  def reset_image(self, showit=True):
-      """Image from pixbufs has changed, reset."""
-      self.showit = showit
-      self._switch_to(self.normal)
-      self.showit = True
-  #end reset_image
+    def reset_image(self, showit=True):
+        """Image from pixbufs has changed, reset."""
+        self.showit = showit
+        self._switch_to(self.normal)
+        self.showit = True
+    #end reset_image
 
-  def is_pressed(self):
-      return self.current != self.normal
-  #end is_pressed
+    def is_pressed(self):
+        return self.current != self.normal
+    #end is_pressed
 
-  def get_really_pressed(self):
-      "Get the pressing state if a key is physically pressed."
-      return self._really_pressed
-  #end get_really_pressed
+    def get_really_pressed(self):
+        "Get the pressing state if a key is physically pressed."
+        return self._really_pressed
+    #end get_really_pressed
 
-  def set_really_pressed(self, value):
-      """Set if a key is physically pressed.
+    def set_really_pressed(self, value):
+        """Set if a key is physically pressed.
 
-      This is different than is_pressed(), which is the pressing state of
-      indicator, not reflect the real key pressing state. Should be set when key
-      event comes in.
-      """
-      self._really_pressed = value
-  #end set_really_pressed
-  # Lint doesn't like @property.setter because of duplicate method names.
-  really_pressed = property \
-    (
-      fget = get_really_pressed,
-      fset = set_really_pressed,
-      doc = "Physically pressed button"
-    )
+        This is different than is_pressed(), which is the pressing state of
+        indicator, not reflect the real key pressing state. Should be set when key
+        event comes in.
+        """
+        self._really_pressed = value
+    #end set_really_pressed
+    # Lint doesn't like @property.setter because of duplicate method names.
+    really_pressed = property \
+      (
+        fget = get_really_pressed,
+        fset = set_really_pressed,
+        doc = "Physically pressed button"
+      )
 
-  def reset_time_if_pressed(self):
-      """Start the countdown now."""
-      if self.is_pressed():
-          self.count_down = time.time()
-      #end if
-  #end reset_time_if_pressed
+    def reset_time_if_pressed(self):
+        """Start the countdown now."""
+        if self.is_pressed():
+            self.count_down = time.time()
+        #end if
+    #end reset_time_if_pressed
 
-  def switch_to(self, name):
-      """Switch to image with this name."""
-      if self.current != self.normal and self.defer_to:
-          self._defer_to(self.current)
-          # Make sure defer_to image will only start counting timeout after self
-          # image has timed out.
-          if self.count_down:
-              self.defer_to.count_down = self.count_down + self.timeout_secs
-          else:
-              self.defer_to.count_down += self.timeout_secs
-          #end if
-      #end if
-      self._switch_to(name)
-  #end switch_to
+    def switch_to(self, name):
+        """Switch to image with this name."""
+        if self.current != self.normal and self.defer_to:
+            self._defer_to(self.current)
+            # Make sure defer_to image will only start counting timeout after self
+            # image has timed out.
+            if self.count_down:
+                self.defer_to.count_down = self.count_down + self.timeout_secs
+            else:
+                self.defer_to.count_down += self.timeout_secs
+            #end if
+        #end if
+        self._switch_to(name)
+    #end switch_to
 
-  def _switch_to(self, name):
-      """Internal, switch to image with this name even if same."""
-      self.set_from_pixbuf(self.pixbufs.get(name))
-      self.current = name
-      self.count_down = None
-      if self.showit:
-          self.show()
-      #end if
-  #end _switch_to
+    def _switch_to(self, name):
+        """Internal, switch to image with this name even if same."""
+        self.set_from_pixbuf(self.pixbufs.get(name))
+        self.current = name
+        self.count_down = None
+        if self.showit:
+            self.show()
+        #end if
+    #end _switch_to
 
-  def switch_to_default(self):
-      """Switch to the default image."""
-      self.count_down = time.time()
-  #end switch_to_default
+    def switch_to_default(self):
+        """Switch to the default image."""
+        self.count_down = time.time()
+    #end switch_to_default
 
-  def empty_event(self):
-      """Sort of a idle event.
+    def empty_event(self):
+        """Sort of a idle event.
 
-      Returns True if image has been changed.
-      """
-      if self.count_down is None:
-          return
-      delta = time.time() - self.count_down
-      if delta > self.timeout_secs:
-          if self.normal.replace('_EMPTY', '') in ('SHIFT', 'ALT', 'CTRL', 'META') and \
-              self.really_pressed:
-              return
-          #end if
-          self.count_down = None
-          self._switch_to(self.normal)
-          return True
-      #end if
-  #end empty_event
+        Returns True if image has been changed.
+        """
+        if self.count_down is None:
+            return
+        delta = time.time() - self.count_down
+        if delta > self.timeout_secs:
+            if self.normal.replace('_EMPTY', '') in ('SHIFT', 'ALT', 'CTRL', 'META') and \
+                self.really_pressed:
+                return
+            #end if
+            self.count_down = None
+            self._switch_to(self.normal)
+            return True
+        #end if
+    #end empty_event
 
-  def _defer_to(self, old_name):
-      """If possible the button is passed on."""
-      if not self.defer_to:
-          return
-      self.defer_to.switch_to(old_name)
-      self.defer_to.switch_to_default()
-  #end _defer_to
+    def _defer_to(self, old_name):
+        """If possible the button is passed on."""
+        if not self.defer_to:
+            return
+        self.defer_to.switch_to(old_name)
+        self.defer_to.switch_to_default()
+    #end _defer_to
 
 #end TwoStateImage
